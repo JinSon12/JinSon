44551e186b963772fecbfc628686279595c39dae
Jin <jinson@uw.edu>
2020-08-30 07:55:48 -0700

    sidebar links styling

--- my-app/src/main/AboutMe.js
+++ my-app/src/main/AboutMe.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import "./main.css";
 
 export default function AboutMe() {
@@ -26,17 +27,25 @@ export default function AboutMe() {
     </div>
   )
 
+  const links = (
+    <div className="d-flex flex-column align-items-center">
+      <Link className="link" to="/">Home</Link>
+      <ul>
+        <li><a className="link" href="https://github.com/JinSon12" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
+        <li><a className="link" href="https://www.linkedin.com/in/jin-son-104042182" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+      </ul>
+    </div>
+  )
+
   return (
     <div className="aboutme d-flex flex-column align-items-center">
       {picture}
       Jin Son
       {contact}
-      <Link to="/">Home</Link>
-      <ul>
-        <li><a href="https://github.com/JinSon12" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
-        <li><a href="https://github.com/JinSon12" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a></li>
-      </ul>
-      <FontAwesomeIcon icon={faGithub} />
+      {links}
+      <p className="small-text">
+        Made with ❤️ by Jin, 2020
+      </p>
     </div>
   )
 }
