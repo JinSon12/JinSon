c118de0d0bae9e671f21df0b7b8f985bdf471b2d
Jin <jinson@uw.edu>
2020-08-31 05:45:52 -0700

    Updates

---
+++ precache-manifest.6cc5daa4b2a33d2154a36807aace5d28.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c345d654a8e327e6c5f71418cfafacd8",
+    "url": "/JinSon/index.html"
+  },
+  {
+    "revision": "3901667891a2d47cfe8c",
+    "url": "/JinSon/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2ebe5aeff058002b3422",
+    "url": "/JinSon/static/css/main.5c4d4f95.chunk.css"
+  },
+  {
+    "revision": "3901667891a2d47cfe8c",
+    "url": "/JinSon/static/js/2.aece5f58.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/JinSon/static/js/2.aece5f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebe5aeff058002b3422",
+    "url": "/JinSon/static/js/main.e60ff834.chunk.js"
+  },
+  {
+    "revision": "a6fa74f95b5d2de9b894",
+    "url": "/JinSon/static/js/runtime-main.d15013b1.js"
+  },
+  {
+    "revision": "0a65bf8172b5124bc888300939c77a59",
+    "url": "/JinSon/static/media/curious.0a65bf81.svg"
+  },
+  {
+    "revision": "e15ca8561b5eca529cd46238340a7068",
+    "url": "/JinSon/static/media/icon-detail.e15ca856.svg"
+  },
+  {
+    "revision": "09e1a12d6f8376271674b9fc74126753",
+    "url": "/JinSon/static/media/responsibility.09e1a12d.svg"
+  },
+  {
+    "revision": "896129aca3c7e4a523e38f1028762e02",
+    "url": "/JinSon/static/media/user.896129ac.svg"
+  }
+]);
