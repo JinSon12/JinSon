41df109ffae75986d6f0cb01adb24ece0939ec59
Jin <jinson@uw.edu>
2020-08-31 03:27:23 -0700

    fixed the mail icon size

--- my-app/src/main/AboutMe.js
+++ my-app/src/main/AboutMe.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import "./main.css";
 
 export default function AboutMe() {
@@ -28,11 +29,12 @@ export default function AboutMe() {
   )
 
   const links = (
-    <div className="d-flex flex-column align-items-center">
+    <div className="d-flex flex-column align-items-center justify-content-between">
       <Link className="link" to="/">Home</Link>
       <ul>
         <li><a className="link" href="https://github.com/JinSon12" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
         <li><a className="link" href="https://www.linkedin.com/in/jin-son-104042182" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+        <li><a className="link" href="mailto:jinson@uw.edu"><FontAwesomeIcon icon={faEnvelope} className="icon-size-sm" /></a></li>
       </ul>
     </div>
   )
